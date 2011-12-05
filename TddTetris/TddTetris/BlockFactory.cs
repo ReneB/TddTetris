52bd1a00a6845970ec2034d23acf2eb867171606
Rene van den Berg <r.van.den.berg@ogd.nl>
2011-12-06 00:12:55 +0100

    Add some features to the BlockFactory to allow it to create randomly shaped blocks

--- TddTetris/TddTetris/BlockFactory.cs
+++ TddTetris/TddTetris/BlockFactory.cs
@@ -2,14 +2,58 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 
 namespace TddTetris
 {
     public class BlockFactory : IBlockFactory
     {
-        public IBlock MakeBlock()
+        public virtual IBlock MakeBlock()
         {
-            return new Block();
+            System.Random RandNum = new System.Random();
+            int randomShapeNumber = RandNum.Next(BlockShapes.Length);
+            return new Block(BlockShapes[randomShapeNumber], colors[randomShapeNumber]);
         }
+
+        public Color[] colors = {
+            Color.White,
+            Color.Blue,
+            Color.Yellow,
+            Color.Red,
+            Color.Green,
+            Color.Purple,
+            Color.Orange
+        };
+
+        public bool[][][] BlockShapes = new bool[7][][] 
+        {
+            new bool [][] {
+                new bool[] {true, true},
+                new bool[] {true, true}
+            },
+            new bool [][] {
+                new bool[] {false, true, true},
+                new bool[] {true, true, false}
+            },
+            new bool [][] {
+                new bool[] {true, true, false},
+                new bool[] {false, true, true},
+            },
+            new bool [][] {
+                new bool[] {true, true, true},
+                new bool[] {true, false, false}
+            },
+            new bool [][] {
+                new bool[] {true, true, true},
+                new bool[] {false, false, true}
+            },
+            new bool [][] {
+                new bool[] {true, true, true},
+                new bool[] {false, true, false}
+            },
+            new bool [][] {
+                new bool[] {true, true, true, true}
+            }
+        };
     }
 }
